999f6a8fd5e61c45dd5e8d47c30184dca1ea40a8
jancris100 <73757410+jancris100@users.noreply.github.com>
2021-08-18 15:28:15 -0600

    Add files via upload

--- src/controlador/ControllerDespacharOrden.java
+++ src/controlador/ControllerDespacharOrden.java
@@ -5,10 +5,82 @@
  */
 package controlador;
 
-/**
- *
- * @author Kevin
- */
-public class ControllerDespacharOrden {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+import javax.swing.JTable;
+import modelo.Conexion;
+import modelo.Pedido;
+import modelo.PedidoDAO;
+import vista.frmDespacharOrden;
+
+
+
+public class ControllerDespacharOrden implements ActionListener {
+    frmDespacharOrden vistaDespachar = new frmDespacharOrden();
+    PedidoDAO dao = new PedidoDAO();
+    Pedido pedido = new Pedido();
     
+    public ControllerDespacharOrden(frmDespacharOrden frm)  {
+        this.vistaDespachar = frm;
+        this.vistaDespachar.btnBuscar.addActionListener(this);
+        this.vistaDespachar.btnDespachar.addActionListener(this);
+        this.vistaDespachar.btnRefrescar.addActionListener(this);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == vistaDespachar.btnDespachar) {
+            try {
+                despacharPedido(1);
+            } catch (Conexion.DataBaseException ex) {
+                Logger.getLogger(ControllerUsuario.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        if (e.getSource() == vistaDespachar.btnBuscar) {
+            try {
+                filtrarTalfiltrarTablaNumProductoDespachado(vistaDespachar.tblProductosDespacho, vistaDespachar.txtBuscar.getText());
+            } catch (Conexion.DataBaseException ex) {
+                Logger.getLogger(ControllerUsuario.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        if(e.getSource()== vistaDespachar.btnRefrescar){
+            try {
+                iniciar();
+                        } catch (Conexion.DataBaseException ex) {
+                Logger.getLogger(ControllerSolicitudesNegadas.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
+
+    public void despacharPedido(int num) throws Conexion.DataBaseException {
+        int numPedido = Integer.parseInt(vistaDespachar.txtNumPedido.getText());
+        int dispatched = num;
+        
+        
+        pedido.setNumPedido(numPedido);
+        pedido.setdespachada(1);
+        
+
+        int r = dao.despacharPedido(pedido);
+
+        if (r > 1) {
+            JOptionPane.showMessageDialog(vistaDespachar, "Solicitud despachada correctamente");
+            dao.iniciarDespacho(vistaDespachar.tblProductosDespacho, "");
+        } else {
+            JOptionPane.showMessageDialog(vistaDespachar, "Solicitud NO tramitada ");
+        }
+    }
+
+    public void iniciar() throws Conexion.DataBaseException {
+        dao.iniciarDespacho(vistaDespachar.tblProductosDespacho, "");
+
+    }
+
+    public void filtrarTalfiltrarTablaNumProductoDespachado(JTable tabla, String filtro) throws Conexion.DataBaseException {
+        dao.filtrarTablaDespacho(tabla, filtro);
+    }
 }
+
